c700e0399f713b0373423db2778f8f1039123bb0
Hafiz24498 <73928018+Hafiz24498@users.noreply.github.com>
2021-11-16 22:04:02 +0700

    Add files via upload

---
+++ Screen/DetailScreen.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+
+
+function DetailScreen( navigation ) {
+    return (
+        <View style={{ flex:1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Home HomeScreen</Text>
+        </View>
+
+    )
+}
+
+export default DetailScreen;
---
+++ Screen/HomeScreen.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+
+
+function HomeScreen( {navigation}) {
+    return (
+        <View style={{ flex:1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Home HomeScreen</Text>
+            <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Detail')}
+      />
+        </View>
+
+    )
+}
+
+export default HomeScreen;
